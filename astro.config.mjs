508188b451bb4fbde98c461723e202ba7f27a3f3
Aranza <65258412+Aranza2001@users.noreply.github.com>
2025-12-09 21:58:11 -0300

    Sacando el link web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://aranza2681.github.io',
+  base: '/Aranza-Portfolio',  // O '/portfolio-personal' según tu repo
   integrations: [tailwind()]
 });
